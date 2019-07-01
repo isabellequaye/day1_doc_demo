5464cb067e0a3822befdd51c697e752c433170cb
Isabelle <isabelleayokor.quaye@gmail.com>
2019-07-01 11:56:49 -0700

    Removed an element and printed

--- day1_doc/MyPlayground.playground/Contents.swift
+++ day1_doc/MyPlayground.playground/Contents.swift
@@ -3,3 +3,10 @@ import UIKit
 var str = "Hello, playground"
 var arr = [1, 3, 5, 7]
 // remove and print the 2nd element
+
+let removed_element = arr.remove(at:1)
+//let (opposite of variable) so it  doesn't change!
+print(removed_element)
+
+
+
